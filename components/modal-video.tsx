2de2582571113f3b1e0000ab2a0c2cdf71830112
Sachin Tripathi <sachintripathi972000@gmail.com>
2024-12-24 04:12:16 +0530

    Update modal-video.tsx

--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -130,8 +130,8 @@ export default function ModalVideo({
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerpolicy="strict-origin-when-cross-origin"
-                                allowfullscreen
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen
                             ></iframe>
                         </DialogPanel>
                     </div>
