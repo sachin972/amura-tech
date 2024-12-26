fd98463a80f54689016e00228674d0b0d8145a00
sachin972 <sachintripathi972000@gmail.com>
2024-12-26 16:19:44 +0530

    updated design

--- components/page-illustration.tsx
+++ components/page-illustration.tsx
@@ -4,13 +4,13 @@ import BlurredShapeGray from "@/public/images/blurred-shape-gray.svg";
 import BlurredShape from "@/public/images/blurred-shape.svg";
 
 export default function PageIllustration({
-  multiple = false,
+    multiple = false,
 }: {
-  multiple?: boolean;
+    multiple?: boolean;
 }) {
-  return (
-    <>
-      <div
+    return (
+        <>
+            {/* <div
         className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/4"
         aria-hidden="true"
       >
@@ -21,35 +21,35 @@ export default function PageIllustration({
           height={594}
           alt="Page illustration"
         />
-      </div>
-      {multiple && (
-        <>
-          <div
-            className="pointer-events-none absolute left-1/2 top-[400px] -z-10 -mt-20 -translate-x-full opacity-50"
-            aria-hidden="true"
-          >
-            <Image
-              className="max-w-none"
-              src={BlurredShapeGray}
-              width={760}
-              height={668}
-              alt="Blurred shape"
-            />
-          </div>
-          <div
-            className="pointer-events-none absolute left-1/2 top-[440px] -z-10 -translate-x-1/3"
-            aria-hidden="true"
-          >
-            <Image
-              className="max-w-none"
-              src={BlurredShape}
-              width={760}
-              height={668}
-              alt="Blurred shape"
-            />
-          </div>
+      </div> */}
+            {multiple && (
+                <>
+                    <div
+                        className="pointer-events-none absolute left-1/2 top-[400px] -z-10 -mt-20 -translate-x-full opacity-50"
+                        aria-hidden="true"
+                    >
+                        <Image
+                            className="max-w-none"
+                            src={BlurredShapeGray}
+                            width={760}
+                            height={668}
+                            alt="Blurred shape"
+                        />
+                    </div>
+                    <div
+                        className="pointer-events-none absolute left-1/2 top-[440px] -z-10 -translate-x-1/3"
+                        aria-hidden="true"
+                    >
+                        <Image
+                            className="max-w-none"
+                            src={BlurredShape}
+                            width={760}
+                            height={668}
+                            alt="Blurred shape"
+                        />
+                    </div>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
