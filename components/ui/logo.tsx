e83487e34f28223866960252e72d6b22ddc54545
sachin972 <sachintripathi972000@gmail.com>
2024-12-26 16:25:58 +0530

    updated logo page

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,7 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/images/amura-final-logo-1.png";
 
-export default function Logo({ height, width }) {
+interface LogoProps {
+    height: number;
+    width: number;
+}
+
+export default function Logo({ height, width }: LogoProps) {
     return (
         <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
             <Image src={logo} alt="Cruip Logo" width={width} height={height} />
