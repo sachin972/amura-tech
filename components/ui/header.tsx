99506820be37b128aa318e4e18f421ee9d2ae9af
sachin972 <sachintripathi972000@gmail.com>
2024-12-28 19:13:41 +0530

    fixed issue

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -263,20 +263,20 @@ export default function Header() {
                             } md:opacity-100 md:translate-y-0 md:pointer-events-auto`}
                         >
                             {[
-                                "Home",
-                                "Industries & Services",
-                                "Careers",
-                                "Contact Us",
+                                { name: "Home", route: "/" },
+                                {
+                                    name: "Industries & Services",
+                                    route: "services",
+                                },
+                                { name: "Careers", route: "careers" },
+                                { name: "Contact Us", route: "contact" },
                             ].map((item) => (
-                                <li key={item}>
+                                <li key={item.name}>
                                     <Link
-                                        href={`/${item
-                                            .toLowerCase()
-                                            .replace(/ & /g, "-")
-                                            .replace(/ /g, "")}`}
+                                        href={`/${item.route.toLowerCase()}`}
                                         className="btn-sm relative bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
                                     >
-                                        {item}
+                                        {item.name}
                                     </Link>
                                 </li>
                             ))}
