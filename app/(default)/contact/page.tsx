ae2c1d6420ecfece3d2aa2c4500ea5293efb014b
sachin972 <sachintripathi972000@gmail.com>
2025-01-03 13:11:55 +0530

    fixed issue

--- app/(default)/contact/page.tsx
+++ app/(default)/contact/page.tsx
@@ -503,7 +503,6 @@ export default function Contact() {
                         width="600"
                         height="450"
                         style={{ border: 0 }}
-                        allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                     ></iframe>
